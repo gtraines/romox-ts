e28389796ca9d11ea904ab2804e7f88b27b19bd2
Graham Traines <gtraines@hotmail.com>
2019-08-29 07:01:00 -0700

    rbxts node modules

--- src/ServerScriptService/AppStart/GameStarter.server.ts
+++ src/ServerScriptService/AppStart/GameStarter.server.ts
@@ -1,7 +1,12 @@
 import { ServerScriptService } from "@rbxts/services";
 
-if (ServerScriptService != undefined) {
-    var folders = ServerScriptService.GetDescendants();
+const scriptFolders = ServerScriptService.GetChildren();
+if (scriptFolders !== undefined){
+    scriptFolders.forEach(entry => print(entry.Name));
+} 
+
+if (ServerScriptService !== undefined) {
+    let folders = ServerScriptService.GetDescendants();
     folders.forEach(element => {
         print(element.Name)
     });
