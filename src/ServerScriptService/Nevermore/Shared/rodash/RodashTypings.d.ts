7246c625843e544e06ee2c11b5c14b6d43444926
Graham Traines <gtraines@hotmail.com>
2019-09-02 02:27:33 -0700

    Rodash typings

--- src/ServerScriptService/Nevermore/Shared/rodash/RodashTypings.d.ts
+++ src/ServerScriptService/Nevermore/Shared/rodash/RodashTypings.d.ts
@@ -1,3 +1,20 @@
+
+export interface IKeyValuePair<TKey, TValue> {
+    Key : TKey;
+    Value : TValue;
+}
+
 export interface IRodash {
-    
+    slice: (source : Array<Table>, first? : number, last? : number, step? : number) => Table[]
+    get: <TKey, TValue>(source : Iterable<Table>, key : TKey, ... keys : TKey[]) => TValue
+    map: <TKey, TValueA, TValueB>(source: Iterable<IKeyValuePair<TKey, TValueA>>,
+        handler: (element: IKeyValuePair<TKey, TValueA>) => TValueB) => Iterable<IKeyValuePair<TKey, TValueB>>
+    mapValues: <TKey, TValueA, TValueB>(source: Iterable<IKeyValuePair<TKey, TValueA>>,
+        handler: (element: IKeyValuePair<TKey, TValueA>) => TValueB) => Iterable<IKeyValuePair<TKey, TValueB>>
+    mapKeys: <TKey, TValueA, TValueB>(source: Iterable<IKeyValuePair<TKey, TValueA>>,
+        handler: (element: TValueA, key: TKey) => TValueB) => Iterable<IKeyValuePair<TKey, TValueB>>
+    flatMap: <TKey, TValue, TUnion>(source : Iterable<IKeyValuePair<TKey, TValue>>, 
+        handler: (element: TValue, key: TKey) => TUnion[]) => TUnion[]
+    shuffle: <Iterable>(source : Iterable) => Iterable
+    filter: <TKey, TValue>(source: Iterable<IKeyValuePair<TKey, TValue>>, handler: (element: TValue, key: TKey) => boolean) => TValue[]
 }
