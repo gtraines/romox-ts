668f43e7896dc214d181270faae06b4c552f98ff
Graham Traines <gtraines@hotmail.com>
2019-09-11 20:59:24 -0700

    Some flag stand impl

--- src/ServerScriptService/FunctionalDomains/FlagStandManager.ts
+++ src/ServerScriptService/FunctionalDomains/FlagStandManager.ts
@@ -10,6 +10,11 @@ import { IPubSub } from '../Nevermore/Shared/Events/PubSubTypings';
 const rq = requireScript("rquery") as IRquery
 const pubSub = requireScript("PubSub") as IPubSub
 
+export enum TransportableArtifactState {
+    AtSpawn = 0,
+    PickedUp = 1
+}
+
 export interface ITransportObjective extends IGameModel {
     WireUpHandlers() : Array<RBXScriptConnection>
     GetOnPickedUpHandler() : (player : Player) => void
@@ -23,23 +28,31 @@ export interface ITransportableArtifact extends IGameModel {
     WireUpHandlers() : Array<IKeyValuePair<string, RBXScriptConnection>>
     GetOnPickedUpHandler() : (player : Player) => void
     GetOnCarrierDiedHandler() : (player : Player) => void
-    GetOnTouchedHandler() : (part : Part) => void
-    State : string
+    GetOnTouchedHandler() : (otherPart : BasePart) => void
+    PickupArtifact(player : Player) : void
+    State : TransportableArtifactState
     TouchedEventConnection? : RBXScriptConnection
     PickedUpEventConnection? : RBXScriptConnection
     CarrierDiedEventConnection? : RBXScriptConnection
     Destroy() : void
 }
 
-export class CtfFlagArtifact extends GameModel implements ITransportableArtifact {
+export interface ICtfFlagArtifact extends ITransportableArtifact {
+    FlagPole : Part
+    FlagBanner : Part
+}
+
+export class CtfFlagArtifact extends GameModel implements ICtfFlagArtifact {
     constructor(gameModel : Model) {
         super(gameModel)
         this.WireUpHandlers()
-        this.State = "AtSpawn"
+        this.State = TransportableArtifactState.AtSpawn
+        this.FlagBanner = gameModel.FindFirstChild("FlagBanner") as Part
+        this.FlagPole = gameModel.FindFirstChild("FlagPole") as Part
     }
     WireUpHandlers(): IKeyValuePair<string, RBXScriptConnection>[] {
         let createdConnections = new Array<IKeyValuePair<string, RBXScriptConnection>>()
-
+        this.TouchedEventConnection = this.FlagPole.Touched.Connect(this.GetOnTouchedHandler())
         return createdConnections
     }
     GetOnPickedUpHandler() : (player : Player) => void {
@@ -50,18 +63,47 @@ export class CtfFlagArtifact extends GameModel implements ITransportableArtifact
     }
     GetOnCarrierDiedHandler() : (player : Player) => void {
         let handler = (player : Player) => {
+            // local flagObject = FlagCarriers[player]
 
         }
         return handler;
     }
-    GetOnTouchedHandler() : (part : Part) => void {
-        let handler = (part : Part) => {
-
+    GetOnTouchedHandler() : (otherPart : BasePart) => void {
+        let handler = (otherPart : BasePart) => {
+            let touchingPersonage = rq.AttachedCharacterOrNil(otherPart as Part)
+            let foundPlayer = rq.GetPlayerFromCharacterOrDescendant(touchingPersonage)
+            if (foundPlayer !== undefined) {
+                let foundHumanoid = rq.GetPersonageOrPlayerHumanoidOrNil(touchingPersonage)
+                if (foundHumanoid.Health <= 0) return;
+                if (this.FlagBanner.BrickColor !== foundPlayer.TeamColor && this.State !== TransportableArtifactState.PickedUp) {
+                    this.PickupArtifact(foundPlayer)
+                }
+            }
         }
         return handler;
     }
+    PickupArtifact(player : Player) {
+        // FlagCarriers[player] = flagModel
+        this.State = TransportableArtifactState.PickedUp
+        let torso = rq.PersonageTorsoOrEquivalent(player.Character as Model)
 
-    State: string;
+        this.FlagPole.Anchored = false
+        this.FlagBanner.Anchored = false
+        this.FlagPole.CanCollide = false
+        this.FlagBanner.CanCollide = false
+
+        let weld = new Instance("Weld", this.FlagPole)
+        weld.Name = "PlayerFlagWeld"
+        weld.Part0 = this.FlagPole
+        weld.Part1 = torso
+        weld.C0 = new CFrame(0, 0, -1)
+
+        // Attach event handlers to player
+        // OnBaseTouched -- remove handler
+    }
+    State: TransportableArtifactState;
+    FlagPole : Part
+    FlagBanner : Part
     TouchedEventConnection?: RBXScriptConnection | undefined;
     PickedUpEventConnection?: RBXScriptConnection | undefined;
     CarrierDiedEventConnection?: RBXScriptConnection | undefined;
